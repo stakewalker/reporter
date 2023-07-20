d1f483f431d9744dc343e55a2db9b7982191f16c
Alexandre Stakonski <115736059+stakewalker@users.noreply.github.com>
2023-07-20 00:53:58 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
-# CryptoCompare API
-chavecrypcomp = 'd74c80c5ed90cb5'+'931dd9247eca5610'+'0866729bf6b78ee'+'78f64bf3ee3e701f9b'
-wp_blog = ('Criptomanchetes', 'dfa79dgs7a8!0HNIHSOTA$##SITOMAKAN###gg')
-wp_website = 'https://satoshidasilva.com/manchetes/'
-tl_pk = 'kz3L1lsaPrtyAQz634bRR65pPuFXenQlFAA:9241661335'
-cutt = '7456a65d6415f220b2a9382cf5235f0ebbbfc'
+chavecrypcomp = 'your_api_key'
+wp_blog = ('blog_name', 'your_api_key')
+wp_website = 'https://your-wp-website.com/'
+tl_pk = 'your_api_key:your_api_key'
+cutt = 'your_api_key'
